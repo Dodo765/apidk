9e3f4bd9c747533780610e6f2cbe977103b0dd7d
Dominik Kawalec <dkawalec76@gmail.com>
2024-06-09 23:30:43 +0200

    das

--- index.js
+++ index.js
@@ -1,58 +1,13 @@
 const express = require("express");
-const { networkInterfaces } = require("os");
 const path = require("path");
 const fs = require("fs");
-const { request } = require("http");
 const app = express();
-const nets = networkInterfaces();
+const bodyParser = require("body-parser");
+const { exec } = require("child_process");
 
 // Server port
 const PORT = 3000;
 
-// //Tally Light
-// app.get("/tallyLight/firmware/firmware.bin", (request, response) => {
-// 	response.download(path.join(__dirname, "../data/tallyLight/firmware/firmware.bin"), "firmware.bin", (err) => {
-// 		if (err) {
-// 			console.error("Problem on download firmware: ", err);
-// 		}
-// 	});
-// });
-
-// app.get("/tallyLight/firmware/version.txt", (request, response) => {
-// 	try {
-// 		const versionPath = path.join(__dirname, "../data/tallyLight/firmware/version.txt");
-// 		const versionContent = fs.readFileSync(versionPath, "utf8");
-
-// 		response.send(versionContent);
-// 		response.status(200);
-// 	} catch (err) {
-// 		console.error("Problem reading version.txt: ", err);
-// 		response.status(500).send("Internal Server Error");
-// 	}
-// });
-
-// //Datavideo
-// app.get("/datavideo/firmware/firmware.bin", (request, response) => {
-// 	response.download(path.join(__dirname, "../data/datavideo/firmware/firmware.bin"), "firmware.bin", (err) => {
-// 		if (err) {
-// 			console.error("Problem on download firmware: ", err);
-// 		}
-// 	});
-// });
-
-// app.get("/datavideo/firmware/version", (request, response) => {
-// 	try {
-// 		const versionPath = path.join(__dirname, "../data/datavideo/firmware/version");
-// 		const versionContent = fs.readFileSync(versionPath, "utf8");
-
-// 		response.send(versionContent);
-// 		response.status(200);
-// 	} catch (err) {
-// 		console.error("Problem reading version: ", err);
-// 		response.status(500).send("Internal Server Error");
-// 	}
-// });
-//Tally Light
 function log(message) {
 	// Pobierz aktualną datę i czas
 	const now = new Date();
@@ -119,6 +74,12 @@ app.get("/datavideo/firmware/version", (request, response) => {
 		console.error("Problem reading version: ", err);
 		response.status(500).send("Internal Server Error");
 	}
+});
+
+app.get("/jdownloader", (req, res) => {
+	const versionPath = path.join(__dirname, "/index.html");
+	res.send(fs.readFileSync(versionPath, "utf8"));
+	res.status(200);
 });
 
 app.all("*", (req, res) => {
